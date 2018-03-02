2646b005a31e2c20f3f68baa8cd05ffcb39437b9
Facundo Petre <facu1p3@gmail.com>
2018-03-01 21:29:07 -0300

    Se quitan librerias de lazy loading, se cambian los logos de los sponsors y se agrega un nuevo integrante al equipo

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $('document').ready(function(){
   Vue.component('team-member', {
     props :['member'],
     template: `<article class="team-article">
-    <div class="lazy hvr-border-fade team-profile-image" v-bind:id="member.teamId" v-bind:data-src="member.imageUrl">
+    <div class="hvr-border-fade team-profile-image" v-bind:id="member.teamId" v-bind:style="{'background-image': 'url('+ member.imageUrl +')'}">
     <div class="team-shadow">
     <p class='team-description'>{{member.personalSummary}}
     <div class="social-line">
@@ -33,7 +33,7 @@ $('document').ready(function(){
 Vue.component('tedx-speaker',{
   props: ['speaker'],
   template:`<article class="speaker-article">
-              <div class="lazy hvr-border-fade profile-image" id="profile-1" v-bind:data-src="speaker.imageUrl"></div><br>
+              <div class="hvr-border-fade profile-image" id="profile-1" v-bind:style="{'background-image': 'url('+ speaker.imageUrl +')'}"></div><br>
               <h5>{{speaker.name}}</h5>
               <footer>
                 <h6>{{speaker.talkTitle}}</h6>
@@ -43,13 +43,13 @@ Vue.component('tedx-speaker',{
 
 Vue.component('tedx-sponsor', {
   props: ['sponsor'],
-  template: `<div class="sponsors-line">
-                <figure class="sponsor-image">
+  template: `
+                <figure class="sponsor-image ">
                 <a v-bind:href="sponsor.webUrl" target="_blank">
-                  <img v-bind:data-src="sponsor.imgUrl" class="lazy" v-bind:alt="sponsor.name">
+                  <img v-bind:src="sponsor.imgUrl" v-bind:class="sponsor.cssClass" v-bind:alt="sponsor.name">
                   </a>
               </figure>
-            </div>`
+            `
 
 })
 var app = new Vue({
@@ -64,11 +64,12 @@ var app = new Vue({
   el: '#app2',
   data: {
     sponsors: [
-      {name:'Gobierno departamental de San José', imgUrl:'images/sponsors/san-jose.jpeg', webUrl:'http://www.sanjose.gub.uy/'},
-      {name:'AMSJ', imgUrl:'images/sponsors/amsj.png', webUrl:'http://www.amsj.com.uy/'},
-      {name:'Marcre', imgUrl:'images/sponsors/marcre.png', webUrl:'http://www.marcre.com.uy/'},
-      {name:'Peraza González', imgUrl:'images/sponsors/peraza-gonzales.png', webUrl:''},
-      {name:'Optica Florida', imgUrl:'images/sponsors/optica-florida.jpeg', webUrl:'http://www.opticaflorida.com.uy/v3/index.php'},
+      {cssClass:'',name:'Gobierno departamental de San José', imgUrl:'images/sponsors/san-jose.png', webUrl:'http://www.sanjose.gub.uy/'},
+      {cssClass:'',name:'AMSJ', imgUrl:'images/sponsors/amsj.png', webUrl:'http://www.amsj.com.uy/'},
+      {cssClass:'utec',name:'UTEC', imgUrl:'images/sponsors/utec.png', webUrl:'https://utec.edu.uy/'},
+      {cssClass:'',name:'Peraza González', imgUrl:'images/sponsors/peraza-gonzales.png', webUrl:''},
+      {cssClass:'',name:'Optica Florida', imgUrl:'images/sponsors/optica-florida.png', webUrl:'http://www.opticaflorida.com.uy/v3/index.php'},
+      {cssClass:'',name:'Marcre', imgUrl:'images/sponsors/marcre.png', webUrl:'http://www.marcre.com.uy/'},
     ]
   }
 })
@@ -81,7 +82,8 @@ var app = new Vue({
       {name: 'Horacio Piriz' , personalSummary:'Hola hola hola ! Here goes your personal summary..', fbUrl:'https://www.fb.com/Horacio.maxx' ,twUrl:'',inUrl:'', lnUrl:'',ghUrl:'', imageUrl:'images/team/horacio.jpeg', teamId:'team-profile-3'},
       {name: 'Matías Laca' , personalSummary:'Hola hola hola ! Here goes your personal summary..', fbUrl:'' ,twUrl:'',inUrl:'', lnUrl:'',ghUrl:'', imageUrl:'images/team/matias.jpeg', teamId:'team-profile-4'},
       {name: 'Gonzalo Prado' , personalSummary:'Hola hola hola ! Here goes your personal summary..', fbUrl:'https://www.fb.com/gonzaloprado8/' ,twUrl:'',inUrl:'https://www.instagram.com/gonzaloprado8/', lnUrl:'',ghUrl:'', imageUrl:'images/team/gonzalo.jpeg', teamId:'team-profile-5'},
-      {name: 'Facundo Petre' , personalSummary:'A curious guy that haves fun researching how things works...', fbUrl:'' ,twUrl:'',inUrl:'https://www.instagram.com/facup3/', lnUrl:'https://www.linkedin.com/in/facundopetre/',ghUrl:'https://github.com/facundop3', imageUrl:'images/team/facundo.jpg', teamId:'team-profile-6'}
+      {name: 'Paola Alcorta' , personalSummary:'Fotografía Músicos 🎶 Teatro 🎭 Danza 💃 Espectáculos en gral 🎬🎤🎨👏', fbUrl:'https://www.facebook.com/paoliita.alcorta' ,twUrl:'',inUrl:'https://www.instagram.com/paoalcortafotografia/', lnUrl:'',ghUrl:'', imageUrl:'images/team/paola.jpeg', teamId:'team-profile-6'},
+      {name: 'Facundo Petre' , personalSummary:'A curious guy that haves fun researching how things works...', fbUrl:'' ,twUrl:'',inUrl:'https://www.instagram.com/facup3/', lnUrl:'https://www.linkedin.com/in/facundopetre/',ghUrl:'https://github.com/facundop3', imageUrl:'images/team/facundo.jpg', teamId:'team-profile-7'}
     ]
   }
 })
